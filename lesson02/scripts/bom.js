e7d6258fe0f24e69f2f4a16afb13130a1a32b028
20mthorpe <tho20019@byui.edu>
2023-09-26 12:14:36 -0600

    DOM activity

--- lesson02/scripts/bom.js
+++ lesson02/scripts/bom.js
@@ -8,7 +8,14 @@ button.addEventListener('click', () => {
     if(input.value) {
         console.log('clicked');
         feedback.textContent = '';
-
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', ()=> {list.removeChild(li); input.focus();});
+        input.value = '';
     } else {
         input.focus();
         feedback.textContent = 'Please enter a Book and Chapter.'
