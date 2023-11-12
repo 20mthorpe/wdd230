023d0cd55afc6fa345748e0d2e8940b971b78fec
20mthorpe <tho20019@byui.edu>
2023-11-11 20:50:14 -0700

    work on weather

---
+++ lesson05/scripts/weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=9154e747c1885dfaeb00fd30637c9ba2';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    //const iconsrc = `https://openweathermap.org/img/w/${data.icon}.png`;
+    //let desc = data.weather[0].desc;
+    //weatherIcon.setAttribute('___', _____);
+    //weatherIcon.setAttribute('___', _____);
+    //captionDesc.textContent = `${desc}`;
+}
+  
+apiFetch();
