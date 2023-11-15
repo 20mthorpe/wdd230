4c29c8514ed213724424850519b9b15b0ab3d4b7
20mthorpe <tho20019@byui.edu>
2023-11-15 15:19:07 -0700

    work

--- scripts/links.js
+++ scripts/links.js
@@ -2,17 +2,24 @@ const linksURL = "../data/links.json";
 const baseURL = "../index.html";
 
 async function getData() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    if (response.ok) {
-        displayWeeks(data.weeks);
-    }
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  if (response.ok) {
+    displayWeeks(data.weeks);
+  }
 }
 function displayWeeks(weeks) {
-    weeks.forEach(week => {
-        //Display the week
-        lessons.forEach(lesson => {
-            //Display each link
-        })
+  weeks.forEach((week) => {
+    //Display the week
+    week.links.forEach((link) => {
+      //Display each link
+      console.log(link);
+      let newLink = document.createElement("a");
+      newLink.setAttribute("href", link.url);
+      newLink.textContent = link.title;
+      console.log(newLink);
     });
+  });
 }
+
+getData();
