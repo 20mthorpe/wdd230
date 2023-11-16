6d19b6b4cfedbd7e65da7c12f4c635bb9b5d8069
20mthorpe <tho20019@byui.edu>
2023-11-16 09:25:36 -0700

    get json links working!

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,6 @@
 const linksURL = "../data/links.json";
 const baseURL = "../index.html";
+const weekList = document.querySelector("#linklist");
 
 async function getData() {
   const response = await fetch(linksURL);
@@ -8,17 +9,35 @@ async function getData() {
     displayWeeks(data.weeks);
   }
 }
+
 function displayWeeks(weeks) {
+
   weeks.forEach((week) => {
     //Display the week
+    let newWeek = document.createElement("li");
+    
+    newWeek.textContent = week.week;
+    weekList.appendChild(newWeek);
+
+    let linkList = document.createElement("ul");
+
     week.links.forEach((link) => {
       //Display each link
-      console.log(link);
+      let newLinkli = document.createElement("li");
+
       let newLink = document.createElement("a");
       newLink.setAttribute("href", link.url);
       newLink.textContent = link.title;
-      console.log(newLink);
+
+      newLinkli.appendChild(newLink);
+      console.log(newLinkli);
+      
+      linkList.appendChild(newLinkli);
+
     });
+
+    newWeek.appendChild(linkList);
+
   });
 }
 
