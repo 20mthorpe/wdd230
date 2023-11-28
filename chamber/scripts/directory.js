5920c570faea8cd322f0063b3cb26553acf3fa23
20mthorpe <tho20019@byui.edu>
2023-11-28 08:49:57 -0700

    work on directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,5 @@
 const dataLink = "data/members.json";
+const cards = document.querySelector('#cards');
 
 async function getMembers(){
 
@@ -7,14 +8,48 @@ async function getMembers(){
 
     if (response.ok) {
         console.log(result);
-        //displayMembers(result);
+        displayMembers(result);
     }
 
 }
 
 async function displayMembers(members)
 {
-    
-    console.log(members[0].name);
+    members.forEach(member => {
+        const card = document.createElement('section');
+
+        const name = document.createElement('h2');
+        name.textContent = `${member.name}`;
+
+        let logo = document.createElement('img');
+
+        logo.setAttribute('src', member.imgURL);
+        logo.setAttribute('alt', `Logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '300');
+        //logo.setAttribute('height', '440');
+
+        card.appendChild(name);
+        card.appendChild(logo);
+        cards.appendChild(card);
+    });
+    //console.log(members[0].name);
 }
-    
+
+getMembers();
+
+
+
+
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.ok) {
+        //console.table(data.prophets);
+        displayProphets(data.prophets);
+    }
+}
+
+//displayProphets(data.prophets);
+
