00e3756c64fc71b477e3c36a35fdf2a5eb2c0289
20mthorpe <tho20019@byui.edu>
2023-12-06 15:01:07 -0700

    add the banner, update the join page

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,25 @@
+const banner = document.querySelector(".banner");
+const bannerContent = document.querySelector("#bannercontent");
+const quitBanner = document.querySelector("#quitbanner");
+
+const now = new Date();
+const day = now.getDay();
+
+console.log(day);
+
+if ([1,2,3].includes(day)){
+    bannerContent.textContent = "Please attend our Chamber of Commerce meet and greet! Wednesdays at 7:00pm";
+}
+else {
+    removeBanner();
+}
+
+function removeBanner(){
+    if (banner) {
+        banner.remove();
+    } else {
+        console.log('banner not found');
+    }
+}
+
+quitBanner.addEventListener('click', removeBanner);
